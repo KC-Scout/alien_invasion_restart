912a897ad395d1f40dd11c247dab4e10a9897944
Connor Foster <foster.clee@gmail.com>
2019-03-07 05:49:26 -0600

    Second Commit

---
+++ alien_invasion.py
@@ -0,0 +1,38 @@
+from settings import Settings
+import pygame
+from pygame.sprite import Group
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+    """ Initialize game and create a screen object."""
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height )
+            ) # Returns Surface
+    
+    # Make a ship, a group of aliens and a group of bullets
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+    
+    # Create a fleet of aliens
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+    
+    pygame.display.set_caption("Alien Invasion")
+
+    # Start the main loop of the game:
+    while True:
+        
+        # Watch for keyboard and mouse events.
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets) 
+        
+
+
+run_game()
+
---
+++ settings.py
@@ -0,0 +1,23 @@
+
+class Settings():
+    """Define the settings for alien invasion."""
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        self.ship_speed_factor = 1.5
+        
+        # Bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3 
+        self.bullet_height = 15
+        self.bullet_color = 255, 0, 0
+        self.bullets_allowed = 3
+        
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
+        
+        
