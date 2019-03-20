cc6ccaaa6217178d78a3f778c6b3d8ac4c097974
Connor Foster <foster.clee@gmail.com>
2019-03-19 19:06:18 -0500

    Collision detection resets ship location and creates a new fleet

--- game_functions.py
+++ game_functions.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from bullet import Bullet
 from alien import Alien
+from time import sleep
 
 def check_key_down_events(event, ai_settings, screen, ship, bullets):
     """Respond to keypresses"""
@@ -126,8 +127,23 @@ def change_fleet_direction(ai_settings, aliens):
         alien.rect.y += ai_settings.fleet_drop_speed
     ai_settings.fleet_direction *= -1
     
+def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+    """Respond to ships being hit by alien."""
+    # Decrement ships left 
+    stats.ships_left -= 1
+    
+    # Empty the list of aliens and bullets
+    aliens.empty()
+    bullets.empty()
+    
+    # Create a new fleet and center the ship.
+    create_fleet(ai_settings, screen, ship, aliens)
+    ship.center_ship()
+    
+    # Pause
+    sleep(0.5)
         
-def update_aliens(ai_settings, ship, aliens):
+def update_aliens(ai_settings, ship, screen, stats, aliens, bullets):
     """
     Check if the fleet is at an edge, and then update the positions
     of all of the aliens in the fleet
@@ -137,7 +153,7 @@ def update_aliens(ai_settings, ship, aliens):
     
     # Look for alien-ship collisions.
     if pygame.sprite.spritecollideany(ship, aliens):
-        print("Ship hit!!")
+        ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
     
         
 
